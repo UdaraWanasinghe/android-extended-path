665a38eb3dd5bbd9bf0c2cd482bf515646c5b0df
Udara Wanasinghe <udara.developer@gmail.com>
2022-11-13 16:36:57 +0530

    set serializable class serial name

--- serializable-path/src/androidTest/java/com/aureusapps/android/serializablepath/SerializablePathInstrumentedTest.kt
+++ serializable-path/src/androidTest/java/com/aureusapps/android/serializablepath/SerializablePathInstrumentedTest.kt
@@ -1,9 +1,6 @@
 package com.aureusapps.android.serializablepath
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.aureusapps.android.serializablepath.commands.AddPath1
-import com.aureusapps.android.serializablepath.commands.LineTo
-import com.aureusapps.android.serializablepath.commands.MoveTo
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.Assert
@@ -20,14 +17,14 @@ class SerializablePathInstrumentedTest {
         path.lineTo(100f, 100f)
         val json = Json.encodeToString(path)
         Assert.assertEquals(
-            """{"commands":[{"type":"${MoveTo::class.qualifiedName}","x":0.0,"y":0.0},{"type":"${LineTo::class.qualifiedName}","x":100.0,"y":100.0}]}""".trimMargin(),
+            """{"commands":[{"type":"MoveTo","x":0.0,"y":0.0},{"type":"LineTo","x":100.0,"y":100.0}]}""".trimMargin(),
             json
         )
         val path2 = SerializablePath.fromJson(json)
         path.addPath(path2)
         val json2 = Json.encodeToString(path)
         Assert.assertEquals(
-            """{"commands":[{"type":"${MoveTo::class.qualifiedName}","x":0.0,"y":0.0},{"type":"${LineTo::class.qualifiedName}","x":100.0,"y":100.0},{"type":"${AddPath1::class.qualifiedName}","path":{"commands":[{"type":"${MoveTo::class.qualifiedName}","x":0.0,"y":0.0},{"type":"${LineTo::class.qualifiedName}","x":100.0,"y":100.0}]}}]}""",
+            """{"commands":[{"type":"MoveTo","x":0.0,"y":0.0},{"type":"LineTo","x":100.0,"y":100.0},{"type":"AddPath1","path":{"commands":[{"type":"MoveTo","x":0.0,"y":0.0},{"type":"LineTo","x":100.0,"y":100.0}]}}]}""",
             json2
         )
     }
