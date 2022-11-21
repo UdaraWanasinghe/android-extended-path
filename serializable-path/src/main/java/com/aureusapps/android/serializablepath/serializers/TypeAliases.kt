7cc9e96f7f31fcdd323ffe67fad2c912da664288
Udara Wanasinghe <udara.developer@gmail.com>
2022-11-21 11:34:04 +0530

    remove path serializer

--- serializable-path/src/main/java/com/aureusapps/android/serializablepath/serializers/TypeAliases.kt
+++ serializable-path/src/main/java/com/aureusapps/android/serializablepath/serializers/TypeAliases.kt
@@ -1,9 +1,6 @@
 package com.aureusapps.android.serializablepath.serializers
 
 import android.graphics.Matrix
-import android.graphics.PointF
 import kotlinx.serialization.Serializable
 
 internal typealias MatrixAsFloatArray = @Serializable(with = MatrixSerializer::class) Matrix
-
-internal typealias PointFAsMap = @Serializable(with = PointFSerializer::class) PointF
