5f1394abdd6ff7991efc3706b308211c5a3579cb
Udara Wanasinghe <udara.developer@gmail.com>
2022-11-21 22:53:21 +0530

    add intersection check unit tests

--- extended-path/src/androidTest/java/com/aureusapps/android/extendedpath/ExtendedPathInstrumentedTest.kt
+++ extended-path/src/androidTest/java/com/aureusapps/android/extendedpath/ExtendedPathInstrumentedTest.kt
@@ -3,7 +3,7 @@ package com.aureusapps.android.extendedpath
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -11,22 +11,35 @@ import org.junit.runner.RunWith
 class ExtendedPathInstrumentedTest {
 
     @Test
-    fun testSerializablePath() {
+    fun testExtendedPathSerialization() {
         val path = ExtendedPath()
         path.moveTo(0f, 0f)
         path.lineTo(100f, 100f)
         val json = Json.encodeToString(path)
-        Assert.assertEquals(
+        assertEquals(
             """{"commands":[{"type":"MoveTo","x":0.0,"y":0.0},{"type":"LineTo","x":100.0,"y":100.0}]}""",
             json
         )
         val path2 = ExtendedPath.fromJson(json)
         path.addPath(path2)
         val json2 = Json.encodeToString(path)
-        Assert.assertEquals(
+        assertEquals(
             """{"commands":[{"type":"MoveTo","x":0.0,"y":0.0},{"type":"LineTo","x":100.0,"y":100.0},{"type":"AddPath1","path":{"commands":[{"type":"MoveTo","x":0.0,"y":0.0},{"type":"LineTo","x":100.0,"y":100.0}]}}]}""",
             json2
         )
+    }
+
+    @Test
+    fun testExtendedPathIntersection() {
+        val path = ExtendedPath()
+        path.moveTo(0f, 0f)
+        path.lineTo(9f, 9f)
+        var doIntersect = path.doIntersect(3f, 3f)
+        assertTrue(doIntersect)
+        doIntersect = path.doIntersect(11f, 11f)
+        assertTrue(doIntersect)
+        doIntersect = path.doIntersect(12f, 12f)
+        assertFalse(doIntersect)
     }
 
 }
